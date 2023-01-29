d1e9313432991d97762b02c382dc5409a2eb899a
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-29 16:56:27 -0300

    fix: fix implemetation of is_anagram with merge sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -28,24 +28,19 @@ def merge(string, start, mid, end):
     left_index = 0
     right_index = 0
 
-    while left_index < len(left_half) and right_index < len(right_half):
-        if left_half[left_index] <= right_half[right_index]:
-            string[start] = left_half[left_index]
+    for index in range(start, end):
+        if left_index >= len(left_half):
+            string[index] = right_half[right_index]
+            right_index += 1
+        elif right_index >= len(right_half):
+            string[index] = left_half[left_index]
+            left_index += 1
+        elif left_half[left_index] < right_half[right_index]:
+            string[index] = left_half[left_index]
             left_index += 1
         else:
-            string[start] = right_half[right_index]
+            string[index] = right_half[right_index]
             right_index += 1
-        start += 1
-
-    while left_index < len(left_half):
-        string[start] = left_half[left_index]
-        left_index += 1
-        start += 1
-
-    while right_index < len(right_half):
-        string[start] = right_half[right_index]
-        right_index += 1
-        start += 1
 
     return string
 
