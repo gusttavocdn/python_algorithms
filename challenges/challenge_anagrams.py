fa391fb49ef4ccc003245c1e45d24a77fdbf8a4f
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-29 16:49:25 -0300

    fix: fix implemetation of is_anagram with merge sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def is_anagram(first_string: str, second_string: str):
-    first_string = merge_sort_string(first_string.lower())
-    second_string = merge_sort_string(second_string.lower())
+    first_string = merge_sort_string(list(first_string.lower()))
+    second_string = merge_sort_string(list(second_string.lower()))
 
     is_anagram = first_string == second_string and (
         first_string != "" and second_string != ""
@@ -8,39 +8,46 @@ def is_anagram(first_string: str, second_string: str):
     return (first_string, second_string, is_anagram)
 
 
-def merge_sort_string(string):
-    string = list(string)
-    if len(string) > 1:
-        mid = len(string) // 2
-        left_half = string[:mid]
-        right_half = string[mid:]
+def merge_sort_string(string, start=0, end=None):
+    if end is None:
+        end = len(string)
 
-        left_half = merge_sort_string(left_half)
-        right_half = merge_sort_string(right_half)
-
-        i = 0
-        j = 0
-        k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                string[k] = left_half[i]
-                i += 1
-            else:
-                string[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            string[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            string[k] = right_half[j]
-            j += 1
-            k += 1
+    if end - start > 1:
+        mid = (start + end) // 2
+        merge_sort_string(string, start, mid)
+        merge_sort_string(string, mid, end)
+        string = merge(string, start, mid, end)
 
     return "".join(string)
 
 
-print(is_anagram("amor", "roma"))
+def merge(string, start, mid, end):
+    left_half = string[start:mid]
+    right_half = string[mid:end]
+
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left_half) and right_index < len(right_half):
+        if left_half[left_index] <= right_half[right_index]:
+            string[start] = left_half[left_index]
+            left_index += 1
+        else:
+            string[start] = right_half[right_index]
+            right_index += 1
+        start += 1
+
+    while left_index < len(left_half):
+        string[start] = left_half[left_index]
+        left_index += 1
+        start += 1
+
+    while right_index < len(right_half):
+        string[start] = right_half[right_index]
+        right_index += 1
+        start += 1
+
+    return string
+
+
+# print(is_anagram("PEDRA", "perda"))
