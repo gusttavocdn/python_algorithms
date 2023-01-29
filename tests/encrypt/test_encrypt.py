fae6dc9db35a10a55bb2433ec63ec8c214784c36
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-29 14:12:05 -0300

    feat: implement test to function encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("33231", "dasdsa")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 2321)
+
+    assert encrypt_message("Naruto", 9) == "oturaN"
+    assert encrypt_message("Naruto", 5) == "turaN_o"
+    assert encrypt_message("Naruto", 4) == "ot_uraN"
