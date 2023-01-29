4966c91ae666f021fc33396e3faf64bda955e8b9
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-28 23:43:35 -0300

    feat: implement function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    try:
+        return sum(
+            target_time in range(_in, out + 1)
+            for _in, out in permanence_period
+        )
+    except TypeError:
+        return None
